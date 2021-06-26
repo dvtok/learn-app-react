eff76ccbe2eece385893e14e743bf56c2d2b447e
Dima Tokarev <dimatokarev@gmail.com>
2021-06-26 13:59:03 +0500

    Урок 5

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props
-    const body = <section>{article.text}</section>
+class Article extends Component {
+    state = {
+        isOpen: true
+    }
 
-    return (
-        <div className="hello" style={{color: 'red'}}>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>дата создания: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+    render () {
+        const {article} = this.props    
+        const body = this.state.isOpen && <section>{article.text}</section>
+    
+        return (
+            <div className="hello" style={{color: 'red'}}>
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick}>
+                        {this.state.isOpen ? 'Закрыть' : 'Открыть'}
+                    </button>
+                </h2>
+                {body}
+                <h3>дата создания: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log('клик')
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
 
 export default Article
