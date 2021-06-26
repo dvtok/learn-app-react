d859b4eb519a397eeac68115560070efeeaf59c2
Dima Tokarev <dimatokarev@gmail.com>
2021-06-26 12:45:23 +0500

    4 урок полностью

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-function Article() {
-    const body = <section>body</section>
+function Article(props) {
+    const {article} = props
+    const body = <section>{article.text}</section>
+
     return (
         <div className="hello" style={{color: 'red'}}>
-            <h2>title1</h2>
+            <h2>{article.title}</h2>
             {body}
-            <h3>дата создания: {(new Date()).toDateString()}</h3>
+            <h3>дата создания: {(new Date(article.date)).toDateString()}</h3>
         </div>
     )
 }
---
+++ src/fixtures.js
@@ -0,0 +1,36 @@
+export let data = [
+    {
+        "id": "56c782f18990ecf954f6e027",
+        "date": "2016-06-09T15:03:23.000Z",
+        "title": "React Is Awesome!!!",
+        "text": "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes. \n We don't make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code. Build encapsulated components that manage their own state, then compose them to make complex UIs. Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.",
+        "comments": [
+            {
+                "id": "qwerqwer",
+                "user": "Gilliam Underwood",
+                "text": "Velit anim deserunt elit velit est fugiat duis eiusmod eu do incididunt ut tempor voluptate. Officia dolor aliqua id anim mollit pariatur id commodo. Laborum minim non ut aliquip commodo est consectetur. Mollit eu aliqua tempor est nulla ullamco irure. Sit non amet et eiusmod cillum ex cillum anim incididunt ad laboris mollit. Sunt quis incididunt elit ea qui non ullamco aliquip consequat voluptate eiusmod est. Irure laboris amet culpa sit aliquip."
+            },
+            {
+                "id": "lkjhsdlfkg",
+                "user": "Dolly Franklin",
+                "text": "Aliquip id nostrud adipisicing irure. Labore reprehenderit ea ex officia ullamco incididunt consequat elit amet quis commodo. Fugiat amet veniam cillum ut aliquip velit est esse minim fugiat eiusmod sint. Commodo ea in culpa deserunt."
+            },
+            {
+                "id": "zxcvzcxvzxcv",
+                "user": "Brennan Atkins",
+                "text": "Nisi sit nisi cillum dolor fugiat sint do nostrud ex cillum cupidatat. Culpa do duis non et excepteur labore dolor culpa qui tempor veniam. Ex labore deserunt qui sit aute ad incididunt do cupidatat eiusmod reprehenderit ad. Qui laborum qui voluptate velit et consectetur ipsum enim dolore minim. Est sint velit tempor reprehenderit. Qui consectetur ad minim consequat."
+            },
+            {
+                "id": "fghjfghjfghj",
+                "user": "Jodi Humphrey",
+                "text": "Non amet amet ut magna culpa dolore consequat occaecat. Commodo adipisicing laboris voluptate enim mollit mollit anim aliquip deserunt nostrud deserunt cillum est. Ad eu cupidatat dolor nostrud et minim id in dolor occaecat ad magna elit. Laboris elit laboris aliquip Lorem reprehenderit id amet reprehenderit laborum minim incididunt cupidatat eiusmod."
+            },
+            {
+                "id": "ertyoertywte",
+                "user": "Joyce Weber",
+                "text": "Non qui Lorem qui commodo sint in esse non aliqua pariatur mollit veniam. Elit labore ad nisi anim adipisicing tempor velit commodo adipisicing ipsum ut. Nostrud cillum aliquip adipisicing id do occaecat est eiusmod adipisicing duis. Magna dolore et non nisi in non cillum officia elit non esse proident irure aute. Proident mollit amet enim dolore eiusmod dolor qui. Eiusmod reprehenderit cillum sit deserunt nostrud enim duis excepteur. Excepteur pariatur sunt in ipsum id minim est mollit."
+            }
+        ]
+    }
+
+]
